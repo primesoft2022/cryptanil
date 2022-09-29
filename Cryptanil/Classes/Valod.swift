606d1a2c093ca5148ce7a68d88223a9092628f5f
Hayk Movsesyan <hayk.movsesyan@primesoft.am>
2022-09-29 18:23:18 +0400

    0.0.1

---
+++ Cryptanil/Classes/Valod.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class Valod {
+    func a(){
+        print("valod")
+    }
+}
---
+++ Example/Pods/Target Support Files/Cryptanil/Cryptanil-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Cryptanil : NSObject
+@end
+@implementation PodsDummy_Cryptanil
+@end
